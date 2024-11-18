59a6bd970f16968834dd019a0acb63bc1b984d81
jakob miesner <jakob.miesner@gmail.com>
2024-11-18 11:26:07 +0100

    preparation for serialization

--- prompt_wrapper.py
+++ prompt_wrapper.py
@@ -24,6 +24,13 @@ class OutputStructure:
         self.sorted_decision_options = sorted_decision_options
         self.first_unstructred_output = first_unstructred_output
 
+    def to_dict(self):
+        return {
+            "sorted_output_components": [component.value for component in self.sorted_output_components],
+            "sorted_decision_options": [option.value for option in self.sorted_decision_options],
+            "first_unstructred_output": self.first_unstructred_output,
+        }
+
 
 class PromptWrapper:
     def __init__(
@@ -47,8 +54,23 @@ class PromptWrapper:
         str += "-----------------------------"
         return str
 
+    def to_dict(self):
+        return {
+            "prompts": self.prompts,
+            "dilemma_identifier": self.dilemma_identifier,
+            "framework_identifier": self.framework_identifier,
+            "output_structure": self.output_structure.to_dict(),
+            "version": self.version,
+        }
+
 
 class Response:
     def __init__(self, prompt: PromptWrapper, decision: DecisionOption):
         self.prompt = prompt
         self.decision = decision
+
+    def to_dict(self):
+        return {
+            "prompt": self.prompt.to_dict(),
+            "decision": self.decision.value,
+        }
