bc3f083605d7ed8a85777d57e709d7a4c83868f1
jakob miesner <jakob.miesner@gmail.com>
2025-03-04 22:46:29 +0100

    updated prompt generation for v1.6

--- commands/generate_prompts.py
+++ commands/generate_prompts.py
@@ -12,16 +12,23 @@ if __name__ == '__main__':
     # Generation forwrapped prompts - v1.6
     prompts_file_path = os.path.join(prompts_folder_path, f"wrapped_prompts_v{VERSION}.json")
     prompts = get_all_possible_prompts()
-    # We found the base_prompt not to have a significant impact, so we willonly use base_prompt_1
-    selected_base_prompt = next(iter(base_prompts))
-    print(f"selected base prompt: {selected_base_prompt}")
-    prompts: list[PromptWrapper] = [x for x in prompts if x.base_prompt_identifier == selected_base_prompt]
+
+    # Selected dilemmas
     prompts: list[PromptWrapper] = [x for x in prompts if x.dilemma.context_identifier in [
         "child_abuse_prevention",
         "public_health",
     ]]
-    # Our current dilemmas do not include the LLM as a subject, thus the egoism framework is not applicable
-    # TODO add later if cost allows it
-    prompts: list[PromptWrapper] = [x for x in prompts if not x.framework_identifier == "ethical_egoism"]
+
+    # We found the base_prompt not to have a significant impact, so we will only use base_prompt_1
+    selected_base_prompt = next(iter(base_prompts))
+    prompts: list[PromptWrapper] = [x for x in prompts if x.base_prompt_identifier == selected_base_prompt]
+
+    # Our current dilemmas do not include the LLM as a subject, thus the egoism framework might not applicable
+    # prompts: list[PromptWrapper] = [x for x in prompts if not x.framework_identifier == "ethical_egoism"]
+
+    # This is a newly added variable we do not yet want to test
+    # Previously the prompt always containted the output structure json and description
+    prompts: list[PromptWrapper] = [x for x in prompts if not x.prompt_has_output_structure_description is True]
+    prompts: list[PromptWrapper] = [x for x in prompts if not x.prompt_has_output_structure_json_schema is True]
     generate_prompt_json(prompts, prompts_file_path)
     print(f"generated {len(prompts)} prompts and saved them to {prompts_file_path}")
