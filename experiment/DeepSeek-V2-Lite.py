bc233d540eb89caf7370d12c2afff7016efdcc00
NAM00 <66006043+NAM00@users.noreply.github.com>
2025-04-19 22:12:38 +0200

    t

---
+++ experiment/DeepSeek-V2-Lite.py
@@ -0,0 +1,53 @@
+import os
+import json
+import transformers
+from transformers import AutoModelForCausalLM, AutoTokenizer
+import os
+from prompt_wrapper import *
+from prompts_json import *
+from prompt_wrapper import PromptWrapper
+
+from typing import List
+
+def query(wrapped_prompt, MODEL_NAME) -> Response:
+    messages = []
+    responses = []
+    try:
+        safeguard = 5  # We never have more than 5 prompts
+        count = 0
+        prompt_tokens = 0
+        completion_tokens = 0
+        for item in wrapped_prompt:
+            prompt = item.get_prompts()
+            messages.append({"role": "system", "content": prompt})
+            kwargs = {}
+            # We add the the response_format either directly or in the second prompt where its asked to parse its ouput.
+
+            pipeline = transformers.pipeline(
+                "text-generation",
+                model=MODEL_NAME,
+                device_map="cuda"
+            )
+            outputs = pipeline(
+                messages,
+                max_new_tokens=1024,
+            )
+            response = outputs[0]["generated_text"][-1]["content"]
+            print(response)
+            print("row ------" + str(count))
+            count = count + 1
+            responses.append(response)
+            with open("./test_DeepSeek-V2-Lite.jsonl", "a") as file:
+                file.write(json.dumps(response) + "\n")
+
+        with open("responses_DeepSeek-V2-Lite.json", "w", encoding="utf-8") as f:
+            json.dump(responses, f, indent=2)
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+
+if __name__ == '__main__':
+    file_path = "data/prompts/wrapped_prompts_v1.6.json"
+    prompts = load_prompts_from_json(file_path)
+
+    query(wrapped_prompt=prompts, MODEL_NAME="deepseek-ai/DeepSeek-V2-Lite")
--- experiment/Llama-3.1-8B-Instruct.py
+++ experiment/Llama-3.1-8B-Instruct.py
@@ -14,10 +14,10 @@ def query(wrapped_prompt, MODEL_NAME) -> Response:
     responses = []
     try:
         safeguard = 5  # We never have more than 5 prompts
-        count = 0
+        count = 442
         prompt_tokens = 0
         completion_tokens = 0
-        for item in wrapped_prompt:
+        for item in wrapped_prompt[441:]:
             prompt = item.get_prompts()
             messages.append({"role": "system", "content": prompt})
             kwargs = {}
@@ -37,10 +37,10 @@ def query(wrapped_prompt, MODEL_NAME) -> Response:
             print("row ------" + str(count))
             count = count + 1
             responses.append(response)
-            with open("./test_3.1_deepseek_1B.jsonl", "a") as file:
+            with open("./test_Llama-3.1-8B-Instruct.jsonl", "a") as file:
                 file.write(json.dumps(response) + "\n")
 
-        with open("responses_3.1_deepseek_1B.json", "w", encoding="utf-8") as f:
+        with open("responses_Llama-3.1-8B-Instruct.json", "w", encoding="utf-8") as f:
             json.dump(responses, f, indent=2)
     except Exception as e:
         print(f"An error occurred: {e}")
@@ -50,4 +50,4 @@ if __name__ == '__main__':
     file_path = "data/prompts/wrapped_prompts_v1.6.json"
     prompts = load_prompts_from_json(file_path)
 
-    query(wrapped_prompt=prompts, MODEL_NAME="deepseek-ai/Janus-Pro-1B")
+    query(wrapped_prompt=prompts, MODEL_NAME="meta-llama/Llama-3.1-8B-Instruct")
